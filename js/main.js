270af62b8ebd836b6c65806ba5b4ec0585c4740c
PedrinXx777 <127054810+PedroHenriqueNascimento777@users.noreply.github.com>
2024-11-19 00:22:23 +0000

    18/11/2024

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "PEDRÂO nutricionista";
+
+var titulo = document.querySelector("h2");
+titulo.textContent = "Pacientes Cadastrados";
+
+var paciente = document.querySelector('#Primeiro-Paciente');
+console.log(paciente);
